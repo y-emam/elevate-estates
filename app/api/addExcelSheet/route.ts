b5d5cfc433f08e6b20531ef3650f05bacc6b239b
y-emam <YasserEmam2002@gmail.com>
2024-11-08 20:37:09 +0200

    the adding to excel sheet functionality is working

---
+++ app/api/addExcelSheet/route.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from "next/server";
+import { google } from "googleapis";
+
+export async function POST(req: NextRequest) {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      destination,
+      propertyType,
+      noBedrooms,
+      delivery,
+    } = await req.json();
+    const createAt = new Date().toISOString();
+
+    const client_email = process.env.SERVICE_ACCOUNT_EMAIL;
+    let private_key = process.env.SERVICE_ACCOUNT_PRIVATE_KEY;
+
+    if (!client_email || !private_key) {
+      return NextResponse.json(
+        { message: "Missing environment variables." },
+        { status: 500 }
+      );
+    }
+
+    private_key = private_key.split(String.raw`\n`).join("\n");
+
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email,
+        private_key,
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+
+    const sheets = google.sheets({ version: "v4", auth });
+
+    const spreadsheetId = process.env.EXCEL_SHEET_ID;
+
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: "Sheet1", // adjust range as needed
+      valueInputOption: "RAW",
+      insertDataOption: "INSERT_ROWS", // Option to insert as new rows
+      requestBody: {
+        values: [
+          [
+            createAt,
+            firstName,
+            lastName,
+            email,
+            phone,
+            destination,
+            propertyType,
+            noBedrooms,
+            delivery,
+          ],
+        ],
+      },
+    });
+
+    return NextResponse.json({ message: "Email sent", response });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { message: "Error updating the excel sheet.", error },
+      { status: 500 }
+    );
+  }
+}
