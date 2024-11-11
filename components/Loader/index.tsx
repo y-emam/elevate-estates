634f8784ed8ae9c3029c837efbeb48429918accc
y-emam <YasserEmam2002@gmail.com>
2024-11-11 20:47:09 +0200

    Added Loader instead of leading screen and added sass to the project

---
+++ components/Loader/index.tsx
@@ -0,0 +1,22 @@
+import "./style.scss";
+function Loader() {
+  return (
+    <div className="scene">
+      <div className="cube-wrapper">
+        <div className="cube">
+          <div className="cube-faces">
+            <div className="cube-face shadow"></div>
+            <div className="cube-face bottom"></div>
+            <div className="cube-face top"></div>
+            <div className="cube-face left"></div>
+            <div className="cube-face right"></div>
+            <div className="cube-face back"></div>
+            <div className="cube-face front"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
